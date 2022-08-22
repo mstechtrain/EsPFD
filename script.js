9d6ea6511dfe6e46b4e1a4ddd08aaf658102756b
Martin Stoyanov <Martin.Stoyanov@alaskaair.com>
2022-08-22 09:02:36 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,39 @@
+let newText = "<h1>hello!</h1>";
+let currentText = document.querySelector("h1");
+console.log(currentText);
+let data = {
+  Pt1: 0,
+  Pt2: 0,
+  Ps1: 0,
+  Ps2: 0,
+};
+
+for (const item in data) {
+  data[item] = Math.random() * 10;
+}
+
+function insertText(text) {
+  //   console.log(text);
+  //   currentText.innerHTML = text;
+}
+
+// fetch("https://jsonplaceholder.typicode.com/todos/1")
+//   .then((response) => response.json())
+//   .then((dataOut) => {
+//     data = dataOut;
+//     currentText.innerHTML = JSON.stringify(data);
+//     console.log(data);
+//   });
+
+//   .then((json) => {
+//     console.log(currentText);
+//     currentText.innerText = json;
+//   });
+
+// console.log(data);
+
+
+setInterval(function(){
+currentText.innerHTML = "<h1> IT WORKS! </h1>";
+
+}, 5000);

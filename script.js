f29301eceaf07ac18507c236a5c71ef6cbfff005
Martin Stoyanov <martin.stoyanov@alaskaair.com>
2022-08-22 10:58:08 -0700

    starting canvas and mockend

--- script.js
+++ script.js
@@ -1,45 +1,49 @@
-let newText = "<h1>hello!</h1>";
-let currentText = document.querySelector("h1");
-console.log(currentText);
-let data = {
+//Canvas Setup//
+
+let canvas = document.getElementById("myCanvas");
+let cC = { aSbg: [], aFbg: [] };
+
+function updatecC() {
+  cC = {
+    aSbg: [0, 0, canvas.height * 0.2, canvas.height],
+    aFbg: [canvas.width * 0.8, 0, canvas.width * 0.2, canvas.height],
+  };
+  console.log(cC);
+}
+
+const resize = () => {
+  ctx.canvas.width = window.innerWidth;
+  ctx.canvas.height = window.innerHeight;
+  updatecC();
+  drawAll();
+};
+
+let ctx = canvas.getContext("2d");
+
+resize();
+window.addEventListener("resize", resize);
+////////////////////////////////
+
+let espData = {
   Pt1: 0,
   Pt2: 0,
   Ps1: 0,
   Ps2: 0,
 };
 
-let options = { acceptAllDevices: true };
-
-function testBT() {
-  navigator.bluetooth
-    .requestDevice(options)
-    .then((device) => {
-      currentText.innerHTML = device.name;
-      console.log(device.name);
-    })
-    .catch((error) => (currentText.innerHTML = error));
+for (const item in espData) {
+  espData[item] = Math.random() * 10;
 }
 
-for (const item in data) {
-  data[item] = Math.random() * 10;
+function drawTapeBG() {
+  ctx.fillStyle = "rgb(0,0,0,.5)";
+  ctx.fillRect(...cC.aSbg);
+  ctx.fillRect(...cC.aFbg);
 }
 
+function drawAll() {
+  ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
+  drawTapeBG();
+}
 
-// fetch("https://jsonplaceholder.typicode.com/todos/1")
-//   .then((response) => response.json())
-//   .then((dataOut) => {
-//     data = dataOut;
-//     currentText.innerHTML = JSON.stringify(data);
-//     console.log(data);
-//   });
-
-//   .then((json) => {
-//     console.log(currentText);
-//     currentText.innerText = json;
-//   });
-
-// console.log(data);
-
-setTimeout(function () {
-  currentText.innerHTML = "<h1> IT WORKS! </h1>";
-}, 1000);
+window.requestAnimationFrame(drawAll);

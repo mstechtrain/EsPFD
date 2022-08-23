80ec5d548bc37ccfd37e069178e25a49dfd8e642
Martin Stoyanov <martin.stoyanov@alaskaair.com>
2022-08-22 19:55:05 -0700

    device motion all values

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-import { cC, ctxaS, ctxaF, ctxeadi} from "./canv.js";
+import { cC, ctxaS, ctxaF, ctxeadi } from "./canv.js";
 import { ticks } from "./ticks.js";
 import { drawAll } from "./draw.js";
 
-window.addEventListener('devicemotion', (event) => {
-  document.getElementById("label").innerText = Math.round(event.acceleration.x);
+window.addEventListener("devicemotion", (event) => {
+  let combinedText =
+    Math.round(event.acceleration.x) +
+    " x     " +
+    Math.round(event.acceleration.y) +
+    " y     " +
+    Math.round(event.acceleration.z) +
+    "  z";
+  document.getElementById("label").innerText = combinedText;
   console.log(`${event.acceleration.x} m/s2`);
 });
 

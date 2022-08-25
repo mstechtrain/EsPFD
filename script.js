deaade732661fc878b277b6d8b6416d0fb38be34
Martin Stoyanov <martin.stoyanov@alaskaair.com>
2022-08-25 11:25:25 -0700

    wakelock added

--- script.js
+++ script.js
@@ -3,13 +3,25 @@ import { ticks } from "./ticks.js";
 import { drawAll } from "./draw.js";
 import { getEspData } from "./airData.js";
 
+var noSleep = new NoSleep();
+
 let btbutton = document.getElementById("eadiLockSwitch");
 btbutton.addEventListener("pointerup", () => {
   navigator.bluetooth.requestDevice({ acceptAllDevices: true });
 });
+
 btbutton.addEventListener("touchend", () => {
-    navigator.bluetooth.requestDevice({ acceptAllDevices: true });
-  });
+  navigator.bluetooth.requestDevice({ acceptAllDevices: true });
+});
+
+document.addEventListener(
+  "click",
+  function enableNoSleep() {
+    document.removeEventListener("click", enableNoSleep, false);
+    noSleep.enable();
+  },
+  false
+);
 
 // function tryBluetooth() {
 //   navigator.bluetooth.requestDevice();
